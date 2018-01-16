983374121d05adb5bd12fad9eea78d7116d288a5
satish <sksatishkumar338@gmail.com>
2018-01-16 12:39:17 +0530

    my fisrt commit

--- app/src/main/java/test/install/itg/gitfirstt/MainActivity.kt
+++ app/src/main/java/test/install/itg/gitfirstt/MainActivity.kt
@@ -1,12 +1,15 @@
 package test.install.itg.gitfirstt
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
+        Log.i("hi","my first commit");
     }
 }
